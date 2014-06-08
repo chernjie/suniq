3b0dc8ad64ef6b0e52b0025276f0aaeb4f8ef557
CJ <lim@chernjie.com>
2014-06-09 00:45:23 +0800

    Fixed dependecies;

--- packager/brew/Library/Formula/suniq.rb
+++ packager/brew/Library/Formula/suniq.rb
@@ -5,6 +5,8 @@ class Suniq < Formula
   url 'https://github.com/chernjie/suniq/archive/1.1.0.tar.gz'
   sha1 '15d3fc356e92f6c7f25b686fd344f4f5ade8b61d'
 
+  depends_on :autoconf
+  depends_on :automake
   def install
     system "./autogen.sh", "--prefix=#{prefix}"
     system "make"
